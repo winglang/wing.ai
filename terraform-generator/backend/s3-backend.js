5fa5fd7ab72a4c8af7fef610ed836993551ea96f
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2024-04-02 15:04:04 +0300

    adding s3 bucket

---
+++ terraform-generator/backend/s3-backend.js
@@ -0,0 +1,23 @@
+exports.Platform = class TFBackend {
+  postSynth(config) {
+    const { BUCKET, REGION, TABLE } = process.env;
+    if (!BUCKET || !REGION || !TABLE) {
+      throw new Error(
+        "BUCKET, REGION or TABLE environment vars are missing for creating an s3 backend.",
+      );
+    }
+
+    console.log(
+      `Creating s3 backend using bucket: ${BUCKET} and table: ${TABLE} in region: ${REGION}`,
+    );
+    config.terraform.backend = {
+      s3: {
+        bucket: BUCKET,
+        region: REGION,
+        key: "/terraform.tfstate",
+        dynamodb_table: TABLE,
+      },
+    };
+    return config;
+  }
+};
